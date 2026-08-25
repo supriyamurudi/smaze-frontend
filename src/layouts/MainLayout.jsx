8f5eec60f9ad4d3b5d2b84a86b6c42eb6c90e7d5
Supriya Murudi <supriyamurudid45@gmail.com>
2026-08-25 11:18:25 +0530

    Updated MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,18 +1,20 @@
+// frontend/src/layouts/MainLayout.jsx
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
 
-      <main className="min-h-screen">
+      {/* ✅ Added padding-top to account for fixed navbar */}
+      <main className="flex-1 pt-20 sm:pt-24 md:pt-28 lg:pt-32">
         <Outlet />
       </main>
 
       <Footer />
-    </>
+    </div>
   );
 };
 
