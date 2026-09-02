ca4900d78e53a985946d8fbcca0ad762735f0353
Supriya Murudi <supriyamurudid45@gmail.com>
2026-09-02 22:37:24 +0530

    Updated main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
 import { HashRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import { Toaster } from "react-hot-toast";
 
 function App() {
+  useEffect(() => {
+    // Ask the browser to make storage persistent
+    if (navigator.storage && navigator.storage.persist) {
+      navigator.storage.persist().then((persistent) => {
+        console.log("Persistent storage granted:", persistent);
+      });
+    }
+  }, []);
+
   return (
     <HashRouter>
       <AppRoutes />
