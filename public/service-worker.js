f1db71724009765dfb88ff54335d3c0d1d9e07b0
Supriya Murudi <supriyamurudid45@gmail.com>
2026-09-02 21:59:55 +0530

    Updated the smaze logo

---
+++ public/service-worker.js
@@ -0,0 +1,36 @@
+/* Smaze Service Worker */
+const CACHE_NAME = "smaze-v1";
+const urlsToCache = ["/", "/index.html"];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }),
+  );
+});
+
+// Fetch assets
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
+
+// Activate Service Worker (Clean up old caches)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        }),
+      );
+    }),
+  );
+});
