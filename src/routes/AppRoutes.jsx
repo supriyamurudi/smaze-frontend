b0743dabf422ee8ab423fd1a754eb8e60978a259
Supriya Murudi <supriyamurudid45@gmail.com>
2026-09-03 23:12:23 +0530

    implemented httpOnly cookies

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -101,7 +101,7 @@ const RequireAuth = ({ allowedRoles, children }) => {
         // ✅ ONLY redirect if we got a 401 (The cookie is truly missing)
         if (error.response && error.response.status === 401) {
           localStorage.removeItem("user");
-          window.location.href = "/#/login";
+          window.location.href = "/#/";
         } else {
           // ✅ If any other error (like network blip) - use saved user data
           const savedUser = localStorage.getItem("user");
